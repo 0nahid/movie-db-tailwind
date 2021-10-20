8f834ccf1106bec247b290e19541398916d2a355
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-10-21 01:17:12 +0600

    added live link

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,9 +11,12 @@ const Home = () => {
           content="You can find the latest movie details on Movie Talks"
         />
       </Helmet>
-      {/* <h1 className="text-6xl text-red-400">Site is in development mode...</h1> */}
+
       <div className=" flex justify-center items-center">
-        <div className="animate-spin ease-linear rounded-full h-16 w-16 border-b-2 border-indigo-500"></div>
+        <h1 className="text-6xl text-indigo-500">
+          Site is in development mode...
+        </h1>
+        <div className="animate-spin ease-linear rounded-full h-12 w-12 border-b-2 border-indigo-500"></div>
       </div>
     </div>
   );

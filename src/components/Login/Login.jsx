c062d3e41004ea9bc04f335aab80a1b75ed5be86
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-10-29 17:30:37 +0600

    added firebase confif

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,60 +1,69 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { FaGoogle } from "react-icons/fa";
-import netflix from "../../Image/netflix.png";
 const Login = () => {
+  const handleEmailSignUp = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
       <Helmet>
         <title>Login - Movie Talks</title>
       </Helmet>
-      <section
-        class="text-gray-600 body-font  "
-        style={{
-          backgroundImage: `url(${netflix})`,
-          backgroundPosition: "center",
-          // backgroundSize: "cover",
-        }}
-      >
-        <div class="container px-5 py-24 mx-auto flex flex-wrap items-center ">
-          <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0   ">
-            <h1 class="title-font font-medium text-3xl text-gray-900">
+      <section className="text-gray-600 body-font ">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap items-center ">
+          <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0  netflix-bg ">
+            <h1 className="title-font font-medium text-3xl text-gray-900">
               Welcome to Movie Talks. Here you can find the latest news of
               movie.
             </h1>
 
-            <p class="leading-relaxed mt-4">
+            <p className="leading-relaxed mt-4">
               To access all the content , you need to be a user of Movie Talks.
             </p>
           </div>
-          <div class="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-            <h2 class="text-gray-900 text-lg font-medium title-font mb-5">
+          <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+            <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
               Sign Up
             </h2>
 
-            <form action="submit">
-              <div class="relative mb-4">
-                <label for="full-name" class="leading-7 text-sm text-gray-600">
+            {/* form part */}
+            <form onSubmit={handleEmailSignUp}>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="full-name"
+                  className="leading-7 text-sm text-gray-600"
+                >
                   Full Name
                 </label>
                 <input
                   type="text"
                   id="full-name"
                   name="full-name"
-                  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  required
+                  autoComplete="false"
                 />
               </div>
-              <div class="relative mb-4">
-                <label for="email" class="leading-7 text-sm text-gray-600">
+              <div className="relative mb-4">
+                <label
+                  htmlFor="email"
+                  className="leading-7 text-sm text-gray-600"
+                >
                   Email
                 </label>
                 <input
                   type="email"
                   id="email"
                   name="email"
-                  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  required
+                  autoComplete="false"
                 />
-                <label for="email" class="leading-7 text-sm text-gray-600">
+                <label
+                  htmlFor="email"
+                  className="leading-7 text-sm text-gray-600"
+                >
                   Password
                 </label>
 
@@ -62,10 +71,12 @@ const Login = () => {
                   type="password"
                   id="password"
                   name="password"
-                  class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  required
+                  autoComplete="false"
                 />
               </div>
-              <button class="text-white w-full bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              <button className="text-white w-full bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                 Sign Up
               </button>
               <p className="text-center ">Or,</p>

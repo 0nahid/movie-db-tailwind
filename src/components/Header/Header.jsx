b0bfd48e4284c3246fd88a73b2774cd4486681d0
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-10-30 00:29:20 +0600

    added route on login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,13 +6,13 @@ const Header = () => {
     <div>
       <header className="text-gray-600 body-font">
         <div className="flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a
-            href="/"
+          <Link
+            to="/"
             className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
           >
             <HiFilm className="h-8 w-8 text-indigo-500" />
             <span className="ml-3 text-xl">Movie Talks</span>
-          </a>
+          </Link>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
             <Link to="/trending" className="mr-5 hover:text-gray-900">
               Trending
@@ -29,10 +29,12 @@ const Header = () => {
               IMDB
             </Link>
           </nav>
-          <button className="inline-flex items-center bg-gray-100 text-indigo-500 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            <Link to="/login">Login</Link>
-            <HiArrowNarrowRight className="w-4 h-4" />
-          </button>
+
+          <Link to="/login">
+            <button className="inline-flex items-center bg-gray-100 text-indigo-500 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+              <HiArrowNarrowRight className="w-4 h-4" /> Login
+            </button>
+          </Link>
         </div>
       </header>
     </div>
